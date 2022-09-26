d48ded6b1ca46b7f2dbca8996c3da6202400e4e1
Oliver Coimbra <olivercoimbra@gmail.com>
2022-09-26 16:12:39 -0400

    setup form with all fields

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Formulario = () => {
   return ( 
-    <h1>Desde el formulario</h1>
+    <Fragment>
+      <h2> Create appointment</h2>
+      <form>
+          <label> Pet Name</label>
+          <input 
+            type="text"
+            name="mascota"
+            className="u-full-width"
+            placeholder="Name of the pet"
+          />
+          <label>Owner Name</label>
+          <input 
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder='Name of the owner'
+          />
+          <label>Date</label>
+          <input 
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          />
+          <label>Hour</label>
+          <input 
+          type="time"
+          name="hora"
+          className="u-full-width"
+          />
+          <label>Symptom</label>
+          <textarea
+          className='u-full-width'
+          name='sintomas'
+          
+          ></textarea>
+          <button
+            type='submit'
+            className='u-full-width button-primary'
+          >Create</button>
+
+      </form>
+
+    </Fragment>
 
    );
 }
